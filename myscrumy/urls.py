9fd1580658d88c697beafe3bd75c010f83388502
EzekielAjayi <ajayiezekiel9000@gmail.com>
2020-05-11 11:59:07 +0100

    Updated the project urls

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ajayiezekiel9000scrumy.urls')),
+    path('ajayiezekiel9000scrumy/', include('ajayiezekiel9000scrumy.urls')),
 ]
