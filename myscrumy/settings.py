3dfc251c675161deef5f05824b238aca07068b46
EzekielAjayi <ajayiezekiel9000@gmail.com>
2020-05-13 21:50:10 +0100

    modified my settings file

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ajayiezekiel9000scrumy.apps.Ajayiezekiel9000ScrumyConfig'
 ]
 
 MIDDLEWARE = [
