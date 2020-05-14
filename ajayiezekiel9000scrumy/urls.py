74bf390c1931de496e080addfcdee5177a99f216
EzekielAjayi <ajayiezekiel9000@gmail.com>
2020-05-14 03:09:21 +0100

    worked on my urls file

--- ajayiezekiel9000scrumy/urls.py
+++ ajayiezekiel9000scrumy/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from ajayiezekiel9000scrumy import views
 
 urlpatterns = [
-    path('', views.get_grading_parameters, name='get_grading_parameters'),
+    path('', views.get_grading_parameters, name="get_grading_parameters"),
 ]
