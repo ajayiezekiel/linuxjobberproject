83a7b6ba5d35c719343685fd3a5fdb602128a22e
EzekielAjayi <ajayiezekiel9000@gmail.com>
2020-05-14 02:25:08 +0100

    trying things out with my django project

--- django-ajayiezekiel9000scrumy/ajayiezekiel9000scrumy/urls.py
+++ django-ajayiezekiel9000scrumy/ajayiezekiel9000scrumy/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_grading_parameters),
+    path('', views.get_grading_parameters, name='get_grading_parameters'),
 ]
