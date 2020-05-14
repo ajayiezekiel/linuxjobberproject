0b32a3e1935fb8d050bd993785ccdb719a13fcf8
EzekielAjayi <ajayiezekiel9000@gmail.com>
2020-05-14 04:27:10 +0100

    made few changes to my file

--- django-ajayiezekiel9000scrumy/setup.py
+++ django-ajayiezekiel9000scrumy/setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name = 'ajayiezekiel9000scrumy',
+    name = 'ajayiezekiel9000',
     version = '0.1',
     packages = find_packages(),
-    author = 'Ezekiel Ajayi',
+    author = 'ajayiezekiel9000scrumy',
     author_email = 'ade@gmail.com',
 )
 
